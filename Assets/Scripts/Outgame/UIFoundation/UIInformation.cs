254cd6b04c4c8f7eaa8300bc4fdcc46d8871987b
k.mitarai <kazmit299@gmail.com>
2023-06-23 22:22:28 +0900

    上げ忘れ

---
+++ Assets/Scripts/Outgame/UIFoundation/UIInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outgame
+{
+    public class UIInformationBase
+    {
+        public T Information<T>() where T : UIInformationBase => this as T;
+    }
+}
